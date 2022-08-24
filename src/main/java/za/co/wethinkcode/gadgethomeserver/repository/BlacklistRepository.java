a377d933cb00a16436d2a84b53bb5ddf758ac5e2
tshiamotman <tshiamo.lephale@gmail.com>
2022-08-24 14:34:53 +0000

    Fixed bug

--- src/main/java/za/co/wethinkcode/gadgethomeserver/repository/BlacklistRepository.java
+++ src/main/java/za/co/wethinkcode/gadgethomeserver/repository/BlacklistRepository.java
@@ -1,9 +1,11 @@
 package za.co.wethinkcode.gadgethomeserver.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import za.co.wethinkcode.gadgethomeserver.models.domain.Blacklist;
 
+@Repository
 public interface BlacklistRepository extends CrudRepository<Blacklist, String> {
     
 }
